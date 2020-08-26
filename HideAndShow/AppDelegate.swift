b7df7739d846515fd0f7c107786d29a95a8a039d
ConnorS98 <connorhuffman16@gmail.com>
2020-08-26 12:40:14 -0500

    Initial Commit

---
+++ HideAndShow/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  HideAndShow
+//
+//  Created by Connor Huffman on 8/26/20.
+//  Copyright © 2020 Connor Huffman. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ HideAndShow/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  HideAndShow
+//
+//  Created by Connor Huffman on 8/26/20.
+//  Copyright © 2020 Connor Huffman. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
